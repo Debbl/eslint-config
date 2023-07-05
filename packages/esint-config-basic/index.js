d7d413ecabe91c907af11bf7693287e26a56abf0
Debbl <me@aiwan.run>
2023-07-05 11:12:48 +0800

    feat(basic): standard

--- packages/esint-config-basic/index.js
+++ packages/esint-config-basic/index.js
@@ -6,8 +6,7 @@ module.exports = {
   },
   reportUnusedDisableDirectives: true,
   extends: [
-    "eslint:recommended",
-    "standard",
+    "./standard",
     "plugin:import/recommended",
     "plugin:eslint-comments/recommended",
     "plugin:jsonc/recommended-with-jsonc",
