1ca368ebddba7e6cdafe4dca374aa762f480b08d
Debbl <me@aiwan.run>
2023-08-22 14:57:22 +0800

    fix: eslint expected string but got `null`

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -6,12 +6,14 @@ const isSolid = isPackageExists("solid-js");
 const isPrettier = isPackageExists("prettier");
 const isTailwindCSS = isPackageExists("tailwindcss");
 
+const allowExtends = [
+  isReact ? "@debbl/eslint-config-react" : null,
+  isVue ? "@debbl/eslint-config-vue" : null,
+  isSolid ? "@debbl/eslint-config-solid" : null,
+  isTailwindCSS ? "@debbl/eslint-config-tailwindcss" : null,
+  isPrettier ? "@debbl/eslint-config-prettier" : null,
+].filter(Boolean);
+
 module.exports = {
-  extends: [
-    isReact ? "@debbl/eslint-config-react" : null,
-    isVue ? "@debbl/eslint-config-vue" : null,
-    isSolid ? "@debbl/eslint-config-solid" : null,
-    isTailwindCSS ? "@debbl/eslint-config-tailwindcss" : null,
-    isPrettier ? "@debbl/eslint-config-prettier" : null,
-  ],
+  extends: allowExtends,
 };
