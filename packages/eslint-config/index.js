45eff46bd28d903b4ee0433146fbc038f8829d25
Debbl <me@aiwan.run>
2023-09-05 13:46:09 +0800

    chore(eslint-config): get package from root

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,24 +1,44 @@
-const { isPackageExists } = require("local-pkg");
+const path = require("node:path");
+const { cwd } = require("node:process");
 
 const isTS = isPackageExists("typescript");
 const isReact = isPackageExists("react");
 const isVue = isPackageExists("vue");
 const isSolid = isPackageExists("solid-js");
-const isPrettier = isPackageExists("prettier");
 const isTailwindCSS = isPackageExists("tailwindcss");
 
 const allowExtends = [
-  "@debbl/eslint-config-basic",
-  isTS ? "@debbl/eslint-config-ts" : null,
   isReact ? "@debbl/eslint-config-react" : null,
   isVue ? "@debbl/eslint-config-vue" : null,
   isSolid ? "@debbl/eslint-config-solid" : null,
   isTailwindCSS ? "@debbl/eslint-config-tailwindcss" : null,
-  isPrettier ? "@debbl/eslint-config-prettier" : null,
 ].filter(Boolean);
+
+if (allowExtends.length === 0) {
+  if (isTS) {
+    allowExtends.push("@debbl/eslint-config-ts");
+  } else {
+    allowExtends.push("@debbl/eslint-config-basic");
+  }
+}
+
+// use prettier
+allowExtends.push("@debbl/eslint-config-prettier");
 
 // eslint-disable-next-line no-console
 console.info("[@debbl/eslint-config] Current running extends:", allowExtends);
+
+// determine if the package is exists
+function isPackageExists(name) {
+  const { dependencies, devDependencies } = require(path.resolve(
+    cwd(),
+    "./package.json"
+  ));
+  return (
+    Object.keys(dependencies || {}).includes(name) ||
+    Object.keys(devDependencies || {}).includes(name)
+  );
+}
 
 module.exports = {
   extends: allowExtends,
