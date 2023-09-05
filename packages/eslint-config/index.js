49e4f6be3be97daabb04126d46ab493af30befee
Debbl <me@aiwan.run>
2023-09-05 12:45:20 +0800

    fix(eslint-config): only ts repo don't have config

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,5 +1,6 @@
 const { isPackageExists } = require("local-pkg");
 
+const isTS = isPackageExists("typescript");
 const isReact = isPackageExists("react");
 const isVue = isPackageExists("vue");
 const isSolid = isPackageExists("solid-js");
@@ -7,6 +8,8 @@ const isPrettier = isPackageExists("prettier");
 const isTailwindCSS = isPackageExists("tailwindcss");
 
 const allowExtends = [
+  "@debbl/eslint-config-basic",
+  isTS ? "@debbl/eslint-config-ts" : null,
   isReact ? "@debbl/eslint-config-react" : null,
   isVue ? "@debbl/eslint-config-vue" : null,
   isSolid ? "@debbl/eslint-config-solid" : null,
