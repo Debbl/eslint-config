09b94f7b36b3a2c7e37789421593e39215ccad58
Debbl <me@aiwan.run>
2023-09-22 14:18:23 +0800

    feat!: custom select

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,48 +1,66 @@
-const path = require("node:path");
-const { cwd } = require("node:process");
+const Basic = require("@debbl/eslint-config-basic");
+const TS = require("@debbl/eslint-config-ts");
+const Prettier = require("@debbl/eslint-config-prettier");
+const React = require("@debbl/eslint-config-react");
+const Vue = require("@debbl/eslint-config-vue");
+const Solid = require("@debbl/eslint-config-solid");
+const TailwindCSS = require("@debbl/eslint-config-tailwindcss");
 
-const isTS = isPackageExists("typescript");
-const isReact = isPackageExists("react");
-const isVue = isPackageExists("vue");
-const isSolid = isPackageExists("solid-js");
-const isTailwindCSS = isPackageExists("tailwindcss");
+function Auto() {
+  const path = require("node:path");
+  const { cwd } = require("node:process");
+  const isTS = isPackageExists("typescript");
+  const isReact = isPackageExists("react");
+  const isVue = isPackageExists("vue");
+  const isSolid = isPackageExists("solid-js");
+  const isTailwindCSS = isPackageExists("tailwindcss");
 
-const allowExtends = [
-  isReact ? "@debbl/eslint-config-react" : null,
-  isVue ? "@debbl/eslint-config-vue" : null,
-  isSolid ? "@debbl/eslint-config-solid" : null,
-  isTailwindCSS ? "@debbl/eslint-config-tailwindcss" : null,
-].filter(Boolean);
+  const allowExtends = [
+    isReact ? "@debbl/eslint-config-react" : null,
+    isVue ? "@debbl/eslint-config-vue" : null,
+    isSolid ? "@debbl/eslint-config-solid" : null,
+    isTailwindCSS ? "@debbl/eslint-config-tailwindcss" : null,
+  ].filter(Boolean);
 
-if (allowExtends.length === 0) {
-  if (isTS) {
-    allowExtends.push("@debbl/eslint-config-ts");
-  } else {
-    allowExtends.push("@debbl/eslint-config-basic");
+  if (allowExtends.length === 0) {
+    if (isTS) {
+      allowExtends.push("@debbl/eslint-config-ts");
+    } else {
+      allowExtends.push("@debbl/eslint-config-basic");
+    }
   }
-}
 
-// use prettier
-allowExtends.push("@debbl/eslint-config-prettier");
+  // use prettier
+  allowExtends.push("@debbl/eslint-config-prettier");
 
-// eslint-disable-next-line no-console
-console.info("[@debbl/eslint-config] Current running extends:", allowExtends);
+  // eslint-disable-next-line no-console
+  console.info("[@debbl/eslint-config] Current running extends:", allowExtends);
 
-// determine if the package is exists
-function isPackageExists(name) {
-  const {
-    dependencies = [],
-    devDependencies = [],
-    peerDependencies = [],
-  } = require(path.resolve(cwd(), "./package.json"));
+  // determine if the package is exists
+  function isPackageExists(name) {
+    const {
+      dependencies = [],
+      devDependencies = [],
+      peerDependencies = [],
+    } = require(path.resolve(cwd(), "./package.json"));
 
-  return (
-    Object.keys(dependencies || {}).includes(name) ||
-    Object.keys(devDependencies || {}).includes(name) ||
-    Object.keys(peerDependencies || {}).includes(name)
-  );
+    return (
+      Object.keys(dependencies || {}).includes(name) ||
+      Object.keys(devDependencies || {}).includes(name) ||
+      Object.keys(peerDependencies || {}).includes(name)
+    );
+  }
+
+  return allowExtends;
 }
 
 module.exports = {
-  extends: allowExtends,
+  Auto,
+  Basic,
+  TS,
+  Prettier,
+  React,
+  Vue,
+  Solid,
+  TailwindCSS,
 };
