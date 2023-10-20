1a6a1ec5daa2568054f2d2ae09d21181066b9594
Debbl <me@aiwan.run>
2023-10-20 15:17:28 +0800

    chore: eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,6 @@
 // @ts-check
-import styleMigrate from "@stylistic/eslint-plugin-migrate";
 import config from "./dist/index.js";
 
-export default config(
-  {
-    vue: true,
-    typescript: true,
-    ignores: ["fixtures", "_fixtures"],
-    overrides: {
-      prettier: {},
-    },
-  },
-  {
-    files: ["src/**/*.ts"],
-    rules: {
-      "sort-keys/sort-keys-fix": "error",
-    },
-  },
-  {
-    files: ["src/configs/*.ts"],
-    plugins: {
-      "style-migrate": styleMigrate,
-    },
-    rules: {
-      "style-migrate/migrate": ["error", { namespaceTo: "style" }],
-    },
-  },
-);
+export default config({
+  typescript: true,
+});
