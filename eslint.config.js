f4b52a6433c5c8927767541a610f3b1d73b06f26
Debbl <me@aiwan.run>
2023-10-20 11:23:20 +0800

    feat: use antfu/eslint-config

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,26 @@
-import { config } from "@debbl/eslint-config";
+// @ts-check
+import styleMigrate from '@stylistic/eslint-plugin-migrate'
+import antfu from './dist/index.js'
 
-export default config();
+export default antfu(
+  {
+    vue: true,
+    typescript: true,
+    ignores: ['fixtures', '_fixtures'],
+  },
+  {
+    files: ['src/**/*.ts'],
+    rules: {
+      'sort-keys/sort-keys-fix': 'error',
+    },
+  },
+  {
+    files: ['src/configs/*.ts'],
+    plugins: {
+      'style-migrate': styleMigrate,
+    },
+    rules: {
+      'style-migrate/migrate': ['error', { namespaceTo: 'style' }],
+    },
+  },
+)
