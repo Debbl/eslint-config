4463fd65541d350608048ccd1705d35a0aa844c2
Brendan Dash <me@aiwan.run>
2023-11-18 21:18:56 +0800

    feat: prettier endOfLine

--- src/configs/prettier.ts
+++ src/configs/prettier.ts
@@ -16,6 +16,7 @@ export function prettier(options: PrettierRequiredOptions): ConfigItem[] {
           "warn",
           {
             quoteProps: "consistent",
+            endOfLine: "lf",
 
             ...options,
           },
