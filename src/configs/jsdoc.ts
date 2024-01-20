501655bdb3acd1c995004dad625e1a2861042cc7
Brendan Dash <me@aiwan.run>
2024-01-20 10:36:47 +0800

    chore: update deps

--- src/configs/jsdoc.ts
+++ src/configs/jsdoc.ts
@@ -6,7 +6,6 @@ export async function jsdoc(): Promise<ConfigItem[]> {
     {
       name: "eslint:jsdoc",
       plugins: {
-        // @ts-expect-error missing types
         jsdoc: await interopDefault(import("eslint-plugin-jsdoc")),
       },
       rules: {
