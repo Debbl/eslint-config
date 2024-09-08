342c0fe511abb3f15b98c9131c1501c04e82e4a6
Brendan Dash <me@aiwan.run>
2024-09-08 21:51:11 +0800

    feat: update perfectionist config

--- src/configs/perfectionist.ts
+++ src/configs/perfectionist.ts
@@ -1,10 +1,10 @@
-import { interopDefault } from "../utils";
 import type { ConfigItem } from "../types";
+import { interopDefault } from "../utils";
 
 /**
- * Optional sort-keys plugin
+ * Perfectionist plugin for props and items sorting.
  *
- * @see https://github.com/azat-io/eslint-plugin-perfectionist/
+ * @see https://github.com/azat-io/eslint-plugin-perfectionist
  */
 export async function perfectionist(): Promise<ConfigItem[]> {
   const pluginPerfectionist = await interopDefault(
@@ -16,6 +16,39 @@ export async function perfectionist(): Promise<ConfigItem[]> {
       name: "eslint/perfectionist/setup",
       plugins: {
         perfectionist: pluginPerfectionist,
+      },
+      rules: {
+        "perfectionist/sort-exports": [
+          "error",
+          { order: "asc", type: "natural" },
+        ],
+        "perfectionist/sort-imports": [
+          "error",
+          {
+            groups: [
+              "builtin",
+              "external",
+              "type",
+              ["internal", "internal-type"],
+              ["parent", "sibling", "index"],
+              ["parent-type", "sibling-type", "index-type"],
+              "side-effect",
+              "object",
+              "unknown",
+            ],
+            newlinesBetween: "ignore",
+            order: "asc",
+            type: "natural",
+          },
+        ],
+        "perfectionist/sort-named-exports": [
+          "error",
+          { order: "asc", type: "natural" },
+        ],
+        "perfectionist/sort-named-imports": [
+          "error",
+          { order: "asc", type: "natural" },
+        ],
       },
     },
   ];
