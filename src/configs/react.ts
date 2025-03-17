5f123cd6f79be6abeffdb5c899747dbdad277bab
Brendan Dash <me@aiwan.run>
2025-03-17 11:59:20 +0800

    feat: update react rules

--- src/configs/react.ts
+++ src/configs/react.ts
@@ -72,25 +72,32 @@ export const react: ReactConfig = async (options): Promise<ConfigItem[]> => {
     overrides = {},
   } = options;
 
-  const [pluginReact, pluginReactHooks, pluginReactRefresh] = await Promise.all(
-    [
+  const [pluginsReact, pluginReactHooks, pluginReactRefresh] =
+    await Promise.all([
       interopDefault(import("@eslint-react/eslint-plugin")),
       interopDefault(import("eslint-plugin-react-hooks")),
       interopDefault(import("eslint-plugin-react-refresh")),
-    ] as const,
-  );
+    ] as const);
 
-  const plugins = pluginReact.configs.all.plugins;
+  const plugins = pluginsReact.configs.all.plugins;
+
+  const {
+    "@eslint-react": pluginReactX,
+    "@eslint-react/dom": pluginReactDom,
+    "@eslint-react/hooks-extra": pluginReactHooksExtra,
+    "@eslint-react/naming-convention": pluginReactNamingConvention,
+    "@eslint-react/web-api": pluginReactWebApi,
+  } = plugins;
 
   const _react: ConfigItem[] = [
     {
       name: "eslint/react/setup",
       plugins: {
-        "react": plugins["@eslint-react"],
-        "react-dom": plugins["@eslint-react/dom"],
-        "react-hooks-extra": plugins["@eslint-react/hooks-extra"],
-        "react-naming-convention": plugins["@eslint-react/naming-convention"],
-        "react-web-api": plugins["@eslint-react/web-api"],
+        "react-x": pluginReactX,
+        "react-dom": pluginReactDom,
+        "react-hooks-extra": pluginReactHooksExtra,
+        "react-naming-convention": pluginReactNamingConvention,
+        "react-web-api": pluginReactWebApi,
         "react-hooks": pluginReactHooks,
         "react-refresh": pluginReactRefresh,
         ...(enableCompiler
@@ -120,24 +127,12 @@ export const react: ReactConfig = async (options): Promise<ConfigItem[]> => {
         },
       },
       rules: {
+        ...pluginReactX.configs.recommended.rules,
+        ...pluginReactDom.configs.recommended.rules,
+        ...pluginReactHooksExtra.configs.recommended.rules,
+        ...pluginReactNamingConvention.configs.recommended.rules,
+        ...pluginReactWebApi.configs.recommended.rules,
         ...pluginReactHooks.configs.recommended.rules,
-
-        // recommended rules from @eslint-react/dom
-        "react-dom/no-children-in-void-dom-elements": "warn",
-        "react-dom/no-dangerously-set-innerhtml": "warn",
-        "react-dom/no-dangerously-set-innerhtml-with-children": "error",
-        "react-dom/no-find-dom-node": "error",
-        "react-dom/no-missing-button-type": "warn",
-        "react-dom/no-missing-iframe-sandbox": "warn",
-        "react-dom/no-namespace": "error",
-        "react-dom/no-render-return-value": "error",
-        "react-dom/no-script-url": "warn",
-        "react-dom/no-unsafe-iframe-sandbox": "warn",
-        "react-dom/no-unsafe-target-blank": "warn",
-
-        // recommended rules react-hooks
-        "react-hooks/exhaustive-deps": "warn",
-        "react-hooks/rules-of-hooks": "error",
 
         // React Refresh
         "react-refresh/only-export-components": "warn",
@@ -148,54 +143,6 @@ export const react: ReactConfig = async (options): Promise<ConfigItem[]> => {
               "react-compiler/react-compiler": "error",
             }
           : {}),
-
-        // recommended rules from @eslint-react/web-api
-        "react-web-api/no-leaked-event-listener": "warn",
-        "react-web-api/no-leaked-interval": "warn",
-        "react-web-api/no-leaked-resize-observer": "warn",
-        "react-web-api/no-leaked-timeout": "warn",
-
-        // recommended rules from @eslint-react
-        "react/ensure-forward-ref-using-ref": "warn",
-        "react/jsx-no-duplicate-props": "warn",
-        "react/jsx-uses-vars": "warn",
-        "react/no-access-state-in-setstate": "error",
-        "react/no-array-index-key": "warn",
-        "react/no-children-count": "warn",
-        "react/no-children-for-each": "warn",
-        "react/no-children-map": "warn",
-        "react/no-children-only": "warn",
-        "react/no-children-to-array": "warn",
-        "react/no-clone-element": "warn",
-        "react/no-comment-textnodes": "warn",
-        "react/no-component-will-mount": "error",
-        "react/no-component-will-receive-props": "error",
-        "react/no-component-will-update": "error",
-        "react/no-context-provider": "warn",
-        "react/no-create-ref": "error",
-        "react/no-default-props": "error",
-        "react/no-direct-mutation-state": "error",
-        "react/no-duplicate-key": "error",
-        "react/no-forward-ref": "warn",
-        "react/no-implicit-key": "warn",
-        "react/no-missing-key": "error",
-        "react/no-nested-components": "error",
-        "react/no-prop-types": "error",
-        "react/no-redundant-should-component-update": "error",
-        "react/no-set-state-in-component-did-mount": "warn",
-        "react/no-set-state-in-component-did-update": "warn",
-        "react/no-set-state-in-component-will-update": "warn",
-        "react/no-string-refs": "error",
-        "react/no-unsafe-component-will-mount": "warn",
-        "react/no-unsafe-component-will-receive-props": "warn",
-        "react/no-unsafe-component-will-update": "warn",
-        "react/no-unstable-context-value": "warn",
-        "react/no-unstable-default-props": "warn",
-        "react/no-unused-class-component-members": "warn",
-        "react/no-unused-state": "warn",
-        "react/prefer-destructuring-assignment": "warn",
-        "react/prefer-shorthand-boolean": "warn",
-        "react/prefer-shorthand-fragment": "warn",
 
         "jsx-quotes": ["error", "prefer-double"],
         "react/react-in-jsx-scope": "off",
