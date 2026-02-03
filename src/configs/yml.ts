159c15e54e96204af4b6d442dfd4ff6f28b6ab35
Brendan Dash <me@aiwan.run>
2026-02-03 22:55:53 +0800

    fix: enhance YAML parser configuration by adding additional error rules for better validation

--- src/configs/yml.ts
+++ src/configs/yml.ts
@@ -26,18 +26,31 @@ export const yml: YmlConfig = async (options) => {
         parser: parserYml,
       },
       rules: {
-        ...(pluginYml.configs['flat/standard'] as any),
-        ...(pluginYml.configs['flat/prettier'] as any),
-
-        'yml/block-mapping': 'error',
-        'yml/block-sequence': 'error',
+        // recommended rules
+        'yml/no-empty-document': 'error',
         'yml/no-empty-key': 'error',
+        'yml/no-empty-mapping-value': 'error',
         'yml/no-empty-sequence-entry': 'error',
         'yml/no-irregular-whitespace': 'error',
-        'yml/plain-scalar': 'error',
-
+        'yml/no-tab-indent': 'error',
         'yml/vue-custom-block/no-parsing-error': 'error',
 
+        // standard rules
+        'yml/block-mapping-colon-indicator-newline': 'error',
+        'yml/block-mapping-question-indicator-newline': 'error',
+        'yml/block-mapping': 'error',
+        'yml/block-sequence-hyphen-indicator-newline': 'error',
+        'yml/block-sequence': 'error',
+        'yml/flow-mapping-curly-newline': 'error',
+        'yml/flow-mapping-curly-spacing': 'error',
+        'yml/flow-sequence-bracket-newline': 'error',
+        'yml/flow-sequence-bracket-spacing': 'error',
+        'yml/indent': 'error',
+        'yml/key-spacing': 'error',
+        'yml/no-multiple-empty-lines': 'error',
+        'yml/no-trailing-zeros': 'error',
+        'yml/plain-scalar': 'error',
+        'yml/quotes': 'error',
         'yml/spaced-comment': 'error',
 
         ...overrides,
